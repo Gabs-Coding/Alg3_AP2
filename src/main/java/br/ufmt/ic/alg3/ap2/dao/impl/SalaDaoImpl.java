31a95dd68b0522b950bae78dbb89de06409d89de
Gabs <gsbiel62@gmail.com>
2023-10-26 16:39:03 -0400

    Modificando nomes do projeto original (AP2) com os nomes do projeto da parte um (AP1).

--- src/main/java/br/ufmt/ic/alg3/ap2/dao/impl/SalaDaoImpl.java
+++ src/main/java/br/ufmt/ic/alg3/ap2/dao/impl/SalaDaoImpl.java
@@ -1,33 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.ufmt.ic.alg3.ap2.dao.impl;
 
 import br.ufmt.ic.alg3.ap2.dao.DAO;
-import br.ufmt.ic.alg3.ap2.model.Aluno;
+import br.ufmt.ic.alg3.ap2.model.Sala;
 import java.util.List;
 
 /**
  *
  * @author Jivago Medeiros
  */
-public class AlunoDaoImpl implements DAO<Aluno> {
+public class SalaDaoImpl implements DAO<Sala> {
 
     @Override
-    public boolean salvar(Aluno t) {
+    public boolean salvar(Sala s) {
         //essa implementação é com vocês!!!
         return false;
     }
 
     @Override
-    public boolean deletar(Aluno t) {
+    public boolean deletar(Sala s) {
         //essa implementação é com vocês!!!
         return false;
     }
 
     @Override
-    public List<Aluno> listar() {
+    public List<Sala> listar() {
         //essa implementação é com vocês!!!
         return null;
     }
